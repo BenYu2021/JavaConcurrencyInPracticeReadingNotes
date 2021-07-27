3c8dd2899bf3331165fb0a5371eedfc18d201855
BenYu2021 <whoyumengmeng@163.com>
2021-07-27 16:46:59 +0800

    init

---
+++ src/main/java/ch01/Test.java
@@ -0,0 +1,13 @@
+package ch01;
+
+/**
+ * @author: ymm
+ * @date: 2021/7/27
+ * @version: 1.0.0
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
